27521143df389f7e7332374d5a5a49cbf636fa4e
tonpasso <wellpasso@gmail.com>
2021-05-17 10:02:34 -0300

    adding basic css

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./ExpenseItem.css";
 
 class ExpenseItem extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {};
   }
-  render() { 
-    return (  
-      <h2>Espense item!</h2>
+  render() {
+    return (
+      <div className="expense-item">
+        <div>March 28th 2021</div>
+        <div className="expense-item__description">
+          <h2>Car Insurance</h2>
+          <div className="expense-item__price">$295.65</div>
+        </div>
+      </div>
     );
   }
 }
- 
+
 export default ExpenseItem;

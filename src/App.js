00f2504b218c541876959f1d5ea66af945002b53
tonpasso <wellpasso@gmail.com>
2021-05-17 09:19:24 -0300

    adding ExpenseItem component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem />
     </div>
   );
 }
---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ExpenseItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  
+      <h2>Espense item!</h2>
+    );
+  }
+}
+ 
+export default ExpenseItem;

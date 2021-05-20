0fe01e8c7d773e45293aa45c2ab18419dd5b76a6
tonpasso <wellpasso@gmail.com>
2021-05-20 11:01:57 -0300

    adding useState for the inputs

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState('');
+  const [date, setDate] = useState('');
+  // the value of the event.target.value it will always be a string, that is why the state is initialized with a empty string
+
+
+  const titleChangeHandler = ({ target: { value } }) => {
+    setTitle(value); 
+    console.log(value);
+  }
+
+  const amountChangeHandler = ({ target: { value } }) => {
+    setAmount(value);
+  }
+
+  const dateChangeHandler = ({ target: { value } }) => {
+    setDate(value);
+  }
+
   return (
     <form>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text'/>
+          <input type='text' onChange={titleChangeHandler} name="title"/>
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01'/>
+          <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler}/>
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min='2019-01-01' max='2022-31-12'/>
+          <input type='date' min='2019-01-01' max='2022-31-12' onChange={dateChangeHandler}/>
         </div>
       </div>
       <div className='new-expense__actions'>
